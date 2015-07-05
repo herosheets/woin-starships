1a611bd50c65d0697d81dbd9c2ee7562a520857b
seiyria <kyle@seiyria.com>
2015-07-05 18:50:48 -0500

    closes #3

--- js/controllers/shipViewCtrl.js
+++ js/controllers/shipViewCtrl.js
@@ -5,4 +5,17 @@ angular.module('woin-starship')
   .controller('ShipViewCtrl', function ShipViewCtrl($scope) {
     $scope = $scope.$parent;
 
+    $scope.calculateWeight = function(weightString) {
+      var split = weightString.split(',').join('').split('-');
+      var min = parseInt(split[0].trim());
+      var max = parseInt(split[1].trim());
+
+      var diff = max - min;
+
+      var currentSpace = $scope.currentSpace();
+      var maxSpace = $scope.maxSpace();
+
+      return min + (currentSpace/maxSpace * diff);
+    };
+
   });
