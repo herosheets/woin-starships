41b101577c88482a0bb02d3cab1c18657202d04b
Morrus <morrus@hotmail.com>
2015-11-02 23:30:54 +0000

    Update powerplantCtrl.js

--- js/controllers/powerplantCtrl.js
+++ js/controllers/powerplantCtrl.js
@@ -4,7 +4,7 @@ angular = require('angular');
 angular.module('woin-starship')
   .controller('PowerplantCtrl', function PowerplantCtrl($scope) {
 
-    var KEY = $scope.KEY = 'Power Plants';
+    var KEY = $scope.KEY = 'Power Plant';
     if(!$scope.ship[KEY]) $scope.ship[KEY] = {};
 
     $scope.powerplantHash = $scope.$parent.powerplantHash;
